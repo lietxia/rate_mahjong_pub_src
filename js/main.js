616e0ad1475e2003daae5a7c56fcbb8da986e49f
lietxia <lietxia@gmail.com>
2020-09-07 23:25:44 +0800

    更新

--- js/main.js
+++ js/main.js
@@ -1,47 +1,46 @@
-var router_obj = {};
-router_obj["index"] = function () {
-    //$("#content_div").load("i.txt")
-    $("#content_div").text("index还没写好");
-};
-router_obj["area"] = function () {
-    $("#content_div").text("area还没写好");
-};
-router_obj["level"] = function () {
-    $("#content_div").text("level还没写好");
-};
-router_obj["ranking"] = function () {
-    $("#content_div").text("ranking还没写好");
-};
+var webobj = {
+    last_hash: "",
+    ce: function () {
+        if (arguments.length % 2 == 0 || arguments.length < 1) { return; }
+        var e = document.createElement(arguments[0]);
+        for (var i = arguments.length - 1; i >= 1; i -= 2) {
+            e.setAttribute(arguments[i - 1], arguments[i]);
+        }
+        return e;
+    },
+    change_jumb: function (title, text) {
+        if (title) { document.getElementById("jumbotron_title").innerText = title; }
+        if (text) { document.getElementById("jumbotron_text").innerText = text; }
+    },
+    router: function () {//onhashchange
+        var hash = window.location.hash.replace(/(^#\/?|\/$)/g, "");
+        var queryarr = hash.split("/");
+        var fn = "main";
+        if (Object.keys(webobj.router_fn).indexOf(queryarr[0]) >= 0) {
+            fn = queryarr.shift();
+        }
+        webobj.router_fn[fn](queryarr);
+    },
+    load_page: function (json_url, tmp_id) {
+        return $.getJSON(json_url, function (json) {
+            document.getElementById("content_div").innerHTML =
+                doT.template(document.getElementById(tmp_id).innerHTML)(json);
+        });
+    },
+    router_fn: {
+        main: function (queryarr) {
 
-function change_j(title, text) {
-    if (title) { document.getElementById("jumbotron_title").innerText = title; }
-    if (text) { document.getElementById("jumbotron_text").innerText = text; }
-}
+        },
+        area: function (queryarr) {
 
-function web_router() {
-    //onhashchange
-    var hash = window.location.hash.replace(/(^#\/?|\/$)/g, "");
-    if (hash == "") {
-        console.log("fn=index\nquery=");
-        return router_obj["index"]();
+        },
+        level: function (queryarr) {
+
+        },
+        ranking: function (queryarr) {
+
+        },
     }
-    var queryarr = hash.split("/");
+};
 
-    if (Object.keys(router_obj).indexOf(queryarr[0]) >= 0) {
-        var fn = queryarr.shift();
-        console.log("fn=" + fn + "\nquery=" + queryarr);
-        return router_obj[fn](queryarr);
-    }
-    console.log("fn=index\nquery=" + queryarr);
-    return router_obj["index"](queryarr);
-}
-web_router();
-function ce() {
-    if (arguments.length % 2 == 0 || arguments.length < 1) { return; }
-    var e = document.createElement(arguments[0]);
-    for (var i = arguments.length - 1; i >= 1; i -= 2) {
-        e.setAttribute(arguments[i - 1], arguments[i]);
-    }
-    return e;
-}
-
+webobj.router();
