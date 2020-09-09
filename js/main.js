cea16131ad17463976cf3a4be2cfc5fe1a0acb34
lietxia <lietxia@gmail.com>
2020-09-09 23:29:45 +0800

    更新

--- js/main.js
+++ js/main.js
@@ -14,91 +14,23 @@ var webobj = {
     fn_setting: {
         main: {
             jumb: ["大数邻", '日本麻将线下成绩展示系统<br />如果你是区域管理员，<a href="#/admin/">按此进入</a>管理页'],
-            json: [
-                {
-                    "group_id": 1231231,
-                    "group_name": "abc",
-                    "timestamp": 1599297253,
-                    "E": {
-                        "QQ": 12345678,
-                        "name": "A",
-                        "score": 25000,
-                        "ranking": 1
-                    },
-                    "S": {
-                        "QQ": 12345678,
-                        "name": "B",
-                        "score": 25000,
-                        "ranking": 2
-                    },
-                    "W": {
-                        "QQ": 12345678,
-                        "name": "C",
-                        "score": 25000,
-                        "ranking": 3
-                    },
-                    "N": {
-                        "QQ": 12345678,
-                        "name": "D",
-                        "score": 25000,
-                        "ranking": 4
-                    }
-                },
-                {
-                    "group": 1231231,
-                    "group_name": "abc",
-                    "timestamp": 1599297253,
-                    "E": {
-                        "QQ": 12345678,
-                        "name": "A",
-                        "score": 25000,
-                        "ranking": 1
-                    },
-                    "S": {
-                        "QQ": 12345678,
-                        "name": "B",
-                        "score": 25000,
-                        "ranking": 2
-                    },
-                    "W": {
-                        "QQ": 12345678,
-                        "name": "C",
-                        "score": 25000,
-                        "ranking": 3
-                    },
-                    "N": {
-                        "QQ": 12345678,
-                        "name": "D",
-                        "score": 25000,
-                        "ranking": 4
-                    }
-                }
-            ]
+            json: "main.json"
         },
         area: {
             jumb: ["区域列表", '各个地区的信息列表'],
-            json: [
-                {
-                    "group_id": 123456,
-                    "group_name": "abc",
-                    "count_members": 123,
-                    "total_battle": 12,
-                    "current_battle": 12
-                },
-                {
-                    "group_id": 123456,
-                    "group_name": "abc",
-                    "count_members": 123,
-                    "total_battle": 12,
-                    "current_battle": 12
-                }
-            ]
+            json: "area.json"
         },
     },
     change_jumb: function () {
-        var data = (arguments.length == 2) ? arguments : webobj.fn_setting[webobj.this_fn].jumb;
-        document.getElementById("jumbotron_title").innerText = data[0];
-        document.getElementById("jumbotron_text").innerHTML = data[1];
+        var data = (arguments.length >= 1) ? arguments
+            : webobj.fn_setting[webobj.this_fn].jumb;
+        if (data.length === 1) { data[1] = false; }
+        if (data[0]) {
+            document.getElementById("jumbotron_title").innerText = data[0];
+        }
+        if (data[1]) {
+            document.getElementById("jumbotron_text").innerHTML = data[1];
+        }
     },
     router: function () {//onhashchange
         var hash = window.location.hash.replace(/(^#\/?|\/$)/g, "");
@@ -107,44 +39,62 @@ var webobj = {
         if (webobj.fn_list.indexOf(queryarr[0]) >= 0) {
             webobj.this_fn = queryarr.shift();
         }
-        if (Object.keys(webobj.before_fn).indexOf(webobj.this_fn) >= 0) {
-            webobj.before_fn[webobj.this_fn](queryarr);
-        }
-        webobj.load_page();
         webobj.change_jumb();
+        if (Object.keys(webobj.before_fn).indexOf(webobj.this_fn) >= 0) {
+            webobj.load_page.apply(null, webobj.before_fn[webobj.this_fn](queryarr));
+        } else {
+            webobj.load_page();
+        }
         if (Object.keys(webobj.after_fn).indexOf(webobj.this_fn) >= 0) {
             webobj.after_fn[webobj.this_fn](queryarr);
         }
         return webobj.last_fn = webobj.this_fn;
     },
     load_page: function () {
-        var obj_or_url = (arguments.length > 0) ? arguments[0] : webobj.fn_setting[webobj.this_fn].json;
         var e = document.getElementById("content_div");
-        var templ = doT.template(document.getElementById("templ_" + webobj.this_fn).text);
-        if (typeof obj_or_url == "object") {
+        var obj_or_url = (arguments.length >= 1) ? arguments[0]
+            : webobj.fn_setting[webobj.this_fn].json;
+        var templ = doT.template(
+            (arguments.length >= 2) ? arguments[1]
+                : document.getElementById("templ_" + webobj.this_fn).text
+        );
+        if (typeof obj_or_url === "object") {
             return e.innerHTML = templ(obj_or_url);
         }
-        if (typeof obj_or_url == "string") {
+        if (typeof obj_or_url === "string") {
             return $.getJSON(obj_or_url, function (json) {
                 e.innerHTML = templ(json);
             });
         }
     },
-    before_fn: {},
+    before_fn: {
+        area: function (queryarr) {
+            console.log("before_FN_AREA", queryarr);
+            var returnarr = [];
+            if (typeof queryarr != "object" || isNaN(queryarr[0])) {
+                return;
+            }
+            webobj.change_jumb(false, "区域列表第" + queryarr[0] + "页");
+            returnarr[0] = "area.json?page=" + queryarr[0];
+            return returnarr;
+        },
+    },
     after_fn: {
         main: function (queryarr) {
-            console.log("after_FN_MAIN");
+            console.log("after_FN_MAIN", queryarr);
         },
         area: function (queryarr) {
-            console.log("after_FN_AREA");
+            console.log("after_FN_AREA", queryarr);
         },
         level: function (queryarr) {
-            console.log("after_FN_LEVEL");
+            console.log("after_FN_LEVEL", queryarr);
         },
         ranking: function (queryarr) {
-            console.log("after_FN_RANKING");
+            console.log("after_FN_RANKING", queryarr);
         },
     }
 };
 
 webobj.router();
+
+正则=(str="")=>/^.(.)\1$/.test(str);
